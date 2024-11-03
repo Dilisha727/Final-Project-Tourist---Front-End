4d29ed6412bb78e280d5318d48a56222dfb83033
Dilisha727 <dilishapriyashan076@gmail.com>
2024-11-03 19:36:41 +0530

    PopupModal Creat

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,7 +11,6 @@ const Welcome = () => {
 export default Welcome;
 */
 
-// src/components/Welcome.js
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Font Awesome for icons
@@ -30,45 +29,46 @@ const Welcome = () => {
   };
 
   return (
-    <div className="container-fluid">
-      <div className="row align-items-center" style={{ height: '100vh' }}>
+    <div className="container-fluid py-5">
+      <div className="row align-items-center">
 
         {/* Left Side - Header, Paragraph, Icons, and Button */}
-        <div className="col-lg-6 d-flex justify-content-center align-items-center flex-column" style={{ paddingLeft: '70px' }}>
-          <h1>Come with us, and let us plan your perfect Sri Lankan adventure!</h1>
-          <p>
+        <div className="col-lg-6 col-md-12 d-flex flex-column justify-content-center align-items-center text-center text-lg-start mb-4 mb-lg-0">
+          <h1 className="display-5 fw-bold">Come with us, and let us plan your perfect Sri Lankan adventure!</h1>
+          <p className="mt-3">
             We specialize in creating tailor-made experiences just for you. Whether you're seeking cultural discoveries, adventure, or relaxation, our AI-powered assistant will help you craft a personalized itinerary that fits your budget, preferences, and travel dates. Trust us to make your Sri Lankan journey unforgettable!
           </p>
 
           {/* Button to trigger the modal */}
-          <button className="btn btn-warning btn-lg" style={{ backgroundColor: '#B7B657', borderColor: '#B7B657', color: '#fff' }} onClick={handleShowModal}>
+          <button className="btn btn-lg mt-3" style={{ backgroundColor: '#B7B657', borderColor: '#B7B657', color: '#fff' }} onClick={handleShowModal}>
             Click to create your dream trip with AI <i className="fas fa-arrow-right"></i>
           </button>
 
-          <div className="welcome-section text-center my-1">
+          {/* Optional Image Below Text */}
+          <div className="welcome-section mt-4">
             <img
               src={require('../image/Margin.png')}
               alt="Welcome"
               className="img-fluid"
-              style={{ maxWidth: '450px', height: 'auto' }}
+              style={{ maxWidth: '100%', height: 'auto' }}
             />
           </div>
         </div>
 
         {/* Right Side - Image */}
-        <div className="col-lg-6 text-center">
+        <div className="col-lg-6 col-md-12 text-center">
           <img
-            src={`${process.env.PUBLIC_URL}/img/1.jpeg`}
+            src={require('../image/Section.png')}
             alt="Ayubowan"
             className="img-fluid"
-            style={{ maxHeight: '90vh' }}
+            style={{ maxHeight: '90vh', width: '100%', objectFit: 'cover' }}
           />
         </div>
 
-        {/* Include the PopupModal Component */}
-        <PopupModal show={showModal} handleClose={handleCloseModal} />
-
       </div>
+
+      {/* Include the PopupModal Component */}
+      <PopupModal show={showModal} handleClose={handleCloseModal} />
     </div>
   );
 };
